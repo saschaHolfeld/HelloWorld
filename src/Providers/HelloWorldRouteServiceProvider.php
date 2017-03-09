dcb686c3c29eb865857b9bd64142bdb6f127c75a
saschaHolfeld <sascha.holfeld@3wfuture.de>
2017-03-09 16:19:05 +0100

    Test asd
    kjhjkh

--- src/Providers/HelloWorldRouteServiceProvider.php
+++ src/Providers/HelloWorldRouteServiceProvider.php
@@ -10,9 +10,9 @@ use Plenty\Plugin\Routing\Router;
 class HelloWorldRouteServiceProvider extends RouteServiceProvider
 {
 	public function map(Router $router)
-	{
-		$router->get('hello', 'HelloWorld\Controllers\ContentController@sayHello');
-	}
+    {
+        $router->get('hello','HelloWorld\Controllers\ContentController@sayHello');
+    }
 }
 
 
