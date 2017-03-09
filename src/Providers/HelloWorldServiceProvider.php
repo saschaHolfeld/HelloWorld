1c45ede5bbb23f887ea03f8b79f604ea7b84ed56
saschaHolfeld <sascha.holfeld@3wfuture.de>
2017-03-09 15:58:07 +0100

    First test

---
+++ src/Providers/HelloWorldServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+// HelloWorld/src/Providers/HelloWorldServiceProvider.php
+
+namespace HelloWorld\Providers;
+
+use Plenty\Plugin\ServiceProvider;
+
+class HelloWorldServiceProvider extends ServiceProvider
+{
+
+	/**
+	 * Register the service provider.
+	 */
+
+	public function register()
+	{
+		$this->getApplication()->register(HelloWorldRouteServiceProvider::class);
+	}
+}
+
+?>
